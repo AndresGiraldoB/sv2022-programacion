9e54f4170ef240c0413199fc30362ecbb9841d3b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2022-09-26 13:45:26 +0200

    Ejercicio 008 mejor tabulado

--- tema01-contactoConCSharp/008-TablaMultiplicar.cs
+++ tema01-contactoConCSharp/008-TablaMultiplicar.cs
@@ -5,29 +5,30 @@ próximo tema veremos cómo hacerlo más eficiente, usando lo que
 llamaremos "bucles". */
 
 /*Alumno: Víctor Raúl (...)
-Cálculo del área de un círculo*/
+Tabla de multiplicar*/
 
 using System;
 
-class Ejercicio8{
+class Ejercicio8
+{
     
-    static void Main() {
+    static void Main() 
+    {
         
         int multiplicando;
         
-            Console.WriteLine("Introduzca que tabla de multiplicar quiere mostrar:");
-            multiplicando = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0} x 0 = {1}", multiplicando, multiplicando*0);
-            Console.WriteLine("{0} x 1 = {1}", multiplicando, multiplicando*1);
-            Console.WriteLine("{0} x 2 = {1}", multiplicando, multiplicando*2);
-            Console.WriteLine("{0} x 3 = {1}", multiplicando, multiplicando*3);
-            Console.WriteLine("{0} x 4 = {1}", multiplicando, multiplicando*4);
-            Console.WriteLine("{0} x 5 = {1}", multiplicando, multiplicando*5);
-            Console.WriteLine("{0} x 6 = {1}", multiplicando, multiplicando*6);
-            Console.WriteLine("{0} x 7 = {1}", multiplicando, multiplicando*7);
-            Console.WriteLine("{0} x 8 = {1}", multiplicando, multiplicando*8);
-            Console.WriteLine("{0} x 9 = {1}", multiplicando, multiplicando*9);
-            Console.WriteLine("{0} x 10 = {1}", multiplicando, multiplicando*10);
-        }
+        Console.WriteLine("Introduzca que tabla de multiplicar quiere mostrar:");
+        multiplicando = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("{0} x 0 = {1}", multiplicando, multiplicando*0);
+        Console.WriteLine("{0} x 1 = {1}", multiplicando, multiplicando*1);
+        Console.WriteLine("{0} x 2 = {1}", multiplicando, multiplicando*2);
+        Console.WriteLine("{0} x 3 = {1}", multiplicando, multiplicando*3);
+        Console.WriteLine("{0} x 4 = {1}", multiplicando, multiplicando*4);
+        Console.WriteLine("{0} x 5 = {1}", multiplicando, multiplicando*5);
+        Console.WriteLine("{0} x 6 = {1}", multiplicando, multiplicando*6);
+        Console.WriteLine("{0} x 7 = {1}", multiplicando, multiplicando*7);
+        Console.WriteLine("{0} x 8 = {1}", multiplicando, multiplicando*8);
+        Console.WriteLine("{0} x 9 = {1}", multiplicando, multiplicando*9);
+        Console.WriteLine("{0} x 10 = {1}", multiplicando, multiplicando*10);
     }
-
+}
